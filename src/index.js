8eb511e6e702bc15f2e79b5027623eec9a6268b0
Murphy Facey (Dev) <facey.murphy1@gmail.com>
2025-09-03 11:58:22 -0500

    feat: update config to make pixel palette lib

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ColorPicker from "./components/ColorPicker.vue";
+import "./style.css";
+
+export { ColorPicker };
+export default ColorPicker;
+export * from "./types/index.js";
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from "vite";
 import Vue from "@vitejs/plugin-vue";
 import Tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "PixelPalette",
+      fileName: "pixel-palette",
+      formats: ["es", "umd"],
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        exports: "named",
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
   plugins: [Vue(), Tailwindcss()],
 });
