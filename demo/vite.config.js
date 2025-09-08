c1e80758029a78cdbaebe3dc85aa45c978850868
Murphy Facey (Dev) <facey.murphy1@gmail.com>
2025-09-08 11:28:20 -0500

    refactor: update base url in vite config

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [vue()],
+    build: {
+      outDir: "dist",
+      assetsDir: "assets",
+    },
+  };
+
+  if (command === "build") {
+    config.base =
+      process.env.NODE_ENV === "production" ? "/pixel-palette/" : "./";
+  }
+
+  return config;
 });
