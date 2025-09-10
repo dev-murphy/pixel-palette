39205bf657df1430f0639af9168b82ed5c2c78ac
Murphy Facey (Dev) <facey.murphy1@gmail.com>
2025-09-10 08:08:29 -0500

    refactor: re-structure vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
-import Vue from "@vitejs/plugin-vue";
-import { fileURLToPath } from "url";
-import path from "path";
+import { resolve } from "path";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import Vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [Vue()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.js"),
+      entry: resolve(__dirname, "src/lib/index.ts"),
       name: "PixelPalette",
       fileName: "pixel-palette",
       formats: ["es", "umd"],
@@ -17,12 +16,10 @@ export default defineConfig({
     rollupOptions: {
       external: ["vue"],
       output: {
-        exports: "named",
         globals: {
           vue: "Vue",
         },
       },
     },
   },
-  plugins: [Vue()],
 });
