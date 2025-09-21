8e4efaf94e392cf28405a73368d870b9265333f9
Murphy Facey (Dev) <facey.murphy1@gmail.com>
2025-09-20 19:00:12 -0500

    refactor: update generated type-defs with unplugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,18 @@ import { resolve } from "path";
 
 import Vue from "@vitejs/plugin-vue";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
+import dts from "unplugin-dts/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [Vue(), libInjectCss()],
+  plugins: [
+    Vue(),
+    libInjectCss(),
+    dts({
+      tsconfigPath: "./tsconfig.build.json",
+      processor: 'vue'
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/lib/index.ts"),
